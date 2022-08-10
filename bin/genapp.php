ea31a35e1a3be1aa2e02b0d5832a50550f6b761c
ehb54 <brookes@uthscsa.edu>
2022-08-10 18:02:09 +0000

    updates - equilbration alpha

--- bin/genapp.php
+++ bin/genapp.php
@@ -10,7 +10,89 @@ class GenApp {
         $this->output = $output;
     }
 
+    function tcpmessagebox( $message ) {
+        $result = (object)[];
+        $msg    = (object)[];
+        $msg->_uuid   = $this->input->_uuid;
+
+        if ( is_string( $message ) ) {
+            $msg->_message = json_decode( $message );
+        } else if ( is_object( $message ) ) {
+            $msg->_message = $message;
+        } else if ( is_array( $message ) ) {
+            $msg->_message = (object)$message;
+        } else {
+            $result->error = 'message must be a json string, array or object';
+            return $result;
+        }
+
+        $msgj = utf8_encode( json_encode( $msg ) );
+        # udpmessage( [ "_textarea" => "message:\n" . json_encode( json_decode( $msgj ), JSON_PRETTY_PRINT ) ] );
+
+        # $this->output->_textarea = "tcpmessagebox message:\n" . json_encode( json_decode( $msgj ), JSON_PRETTY_PRINT ) . "\n";
+
+        # open socket
+        if ( !($socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) ) {
+            $result->error = 'socket create error : ' . socket_strerror( socket_last_error() );
+            return $result;
+        }
+        if ( !socket_connect( $socket, $this->input->_tcphost, intval( $this->input->_tcpport ) ) ) {
+            $result->error = 'socket connect error : ' . socket_strerror( socket_last_error() );
+            return $result;
+        }
+
+        # send message
+
+        if ( strlen( $msgj ) != ( $bytes_sent = socket_write( $socket, $msgj, strlen( $msgj ) ) ) ) {
+            $result->error = "socket send error bytes sent : $bytes_sent : " . socket_strerror( socket_last_error() );
+            return $result;
+        }
+        socket_close( $socket );
+        return "ok";
+    }
+
     function tcpmessage( $message ) {
+        $result = (object)[];
+        $msg    = (object)[];
+
+        if ( is_string( $message ) ) {
+            $msg = json_decode( $message );
+        } else if ( is_object( $message ) ) {
+            $msg = $message;
+        } else if ( is_array( $message ) ) {
+            $msg = (object)$message;
+        } else {
+            $result->error = 'message must be a json string, array or object';
+            return $result;
+        }
+
+        $msg->_uuid   = $this->input->_uuid;
+
+        $msgj = utf8_encode( json_encode( $msg ) );
+
+        # open socket
+        if ( !($socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) ) {
+            $result->input = json_encode( $this->input );
+            $result->error = 'socket create error : ' . socket_strerror( socket_last_error() );
+            return $result;
+        }
+        if ( !socket_connect( $socket, $this->input->_tcphost, intval( $this->input->_tcpport ) ) ) {
+            $result->input = json_encode( $this->input );
+            $result->error = 'socket connect error : ' . socket_strerror( socket_last_error() );
+            return $result;
+        }
+
+        # send message
+
+        if ( strlen( $msgj ) != ( $bytes_sent = socket_write( $socket, $msgj, strlen( $msgj ) ) ) ) {
+            $result->error = "socket send error bytes sent : $bytes_sent : " . socket_strerror( socket_last_error() );
+            return $result;
+        }
+        socket_close( $socket );
+        return "ok";
+    }
+
+    function udpmessagebox( $message ) {
         $result = (object)[];
         $msg    = (object)[];
         $msg->_uuid   = $this->input->_uuid;
@@ -28,25 +110,15 @@ class GenApp {
 
         $msgj = utf8_encode( json_encode( $msg ) );
 
-        # $this->output->_textarea = "message:\n" . json_encode( json_decode( $msgj ), JSON_PRETTY_PRINT );
+        # $this->output->_textarea = "udp message:\n" . json_encode( json_decode( $msgj ), JSON_PRETTY_PRINT );
 
         # open socket
-        if ( !($socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) ) {
-            $result->error = 'socket create error : ' . socket_strerror( socket_last_error() );
-            return $result;
-        }
-        if ( !socket_connect( $socket, $this->input->_tcphost, intval( $input->_tcpport ) ) ) {
-            $result->error = 'socket connect error : ' . socket_strerror( socket_last_error() );
-            return $result;
-        }
+        $socket = socket_create( AF_INET, SOCK_DGRAM, 0 );
 
         # send message
 
-        if ( strlen( $msgj ) != ( $bytes_sent = socket_write( $socket, $msgj, strlen( $msgj ) ) ) ) {
-            $result->error = "socket send error bytes sent : $bytes_sent : " . socket_strerror( socket_last_error() );
-            return $result;
-        }
-        socket_close( $socket );
+        socket_sendto( $socket, $msgj, strlen( $msgj ), 0, $this->input->_udphost, intval( $this->input->_udpport ) );
+
         return "ok";
     }
 
@@ -56,15 +128,17 @@ class GenApp {
         $msg->_uuid   = $this->input->_uuid;
 
         if ( is_string( $message ) ) {
-            $msg->_message = json_decode( $message );
+            $msg = json_decode( $message );
         } else if ( is_object( $message ) ) {
-            $msg->_message = $message;
+            $msg = $message;
         } else if ( is_array( $message ) ) {
-            $msg->_message = (object)$message;
+            $msg = (object)$message;
         } else {
             $result->error = 'message must be a json string, array or object';
             return $result;
         }
+
+        $msg->_uuid   = $this->input->_uuid;
 
         $msgj = utf8_encode( json_encode( $msg ) );
 
