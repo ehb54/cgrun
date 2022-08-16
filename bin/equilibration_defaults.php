65da3177888d8e700340e1ce97deb044f337e94f
ehb54 <brookes@uthscsa.edu>
2022-08-16 17:11:01 +0000

    equilibration - get/adjust inp values

---
+++ bin/equilibration_defaults.php
@@ -0,0 +1,36 @@
+#!/usr/local/bin/php
+<?php
+{};
+
+$request = json_decode( file_get_contents( "php://stdin" ) );
+$result  = (object)[];
+
+function error_exit( $msg ) {
+    global $result;
+    $result->_error = $msg;
+    echo json_encode( $result );
+    exit;
+}
+
+if ( $request === NULL ) {
+    error_exit( "Invalid JSON input provided" );
+}
+
+## find step4_
+
+$ifile = __DIR__ . "/../results/users/$request->_logon/$request->_project/charmm-gui/namd/step4_equilibration.inp";
+
+if ( file_exists( $ifile ) ) {
+    $result->{"sparams-min_steps"}   = intval( `grep -Pi '^minimize ' $ifile  | awk '{ print \$2 }'` );
+    $result->{"sparams-run_steps"}   = intval( `grep -Pi '^run ' $ifile  | awk '{ print \$2 }'` );
+    $result->{"sparams-dcdfreq"}     = intval( `grep -Pi '^dcdfreq ' $ifile  | awk '{ print \$2 }'` );
+    $result->{"sparams-temperature"} = floatval( `grep -Pi '^set temp ' $ifile | awk '{ print \$3 }'` );
+} else {
+    $result->error = "$ifile does not exist";
+}   
+
+$result->_status = "load_defaults received ok json";
+echo json_encode( $result );
+exit;
+
+
